87902f2e948af1c8c8986af620d0ac2a7638f5c9
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-02-08 12:44:06 +0530

    Update CalculatorDs.java

--- src/main/java/pk/CalculatorDs.java
+++ src/main/java/pk/CalculatorDs.java
@@ -35,49 +35,54 @@ public class CalculatorDs extends Calculator{
     void rem(int value){
         setValue(getValue()%value);
     }
+    static void print(){
+        String s = "Result: "+c.getValue();
+        LOGGER.info(s);
+    }
     private static final Logger LOGGER =  Logger.getLogger("InfoLogging");
     static Calculator c = new CalculatorDs();
+    static String msg = "Enter a value: ";
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         int n = 0;
         do{
-            LOGGER.info("Enter a value: ");
+            LOGGER.info(msg);
             int data = sc.nextInt();
             c.setValue(data);
             int l = 0;
             do{
-                LOGGER.info("1.Add 2.subract 3.divide 4.multiply 5.clear");
+                LOGGER.info("1.Add 2.subract 3.divide 4.multiply 5.clear 6.exit");
                 int m = sc.nextInt();
                 if(m == 1){
-                    LOGGER.info("Enter a value: ");
+                    LOGGER.info(msg);
                     int value = sc.nextInt();
                     c.add(value);
-                    String s = "Result: "+c.getValue();
-                    LOGGER.info(s);
+                    print();
                 }
                 else if(m == 2){
-                    LOGGER.info("Enter a value: ");
+                    LOGGER.info(msg);
                     int value = sc.nextInt();
                     c.sub(value);
-                    String s = "Result: "+c.getValue();
-                    LOGGER.info(s);
+                    print();
                 }
                 else if(m == 3){
-                    LOGGER.info("Enter a value: ");
+                    LOGGER.info(msg);
                     int value = sc.nextInt();
                     c.div(value);
-                    String s = "Result: "+c.getValue();
-                    LOGGER.info(s);
+                    print();
                 }
                 else if(m == 4){
-                    LOGGER.info("Enter a value: ");
+                    LOGGER.info(msg);
                     int value = sc.nextInt();
                     c.mul(value);
-                    String s = "Result: "+c.getValue();
-                    LOGGER.info(s);
+                    print();
                 }
                 else if(m == 5){
                     c.setValue(0);
+                    l = 1;
+                }
+                else if(m == 6){
+                    n=1;
                     l = 1;
                 }
             }while(l==0);
