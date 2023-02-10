03b3320c06c5e0f9066078b1cf62fdfc4c9717d2
prasanna-761 <123439582+prasanna-761@users.noreply.github.com>
2023-02-10 12:58:14 +0530

    Update CalculatorDs.java

--- src/main/java/pk/CalculatorDs.java
+++ src/main/java/pk/CalculatorDs.java
@@ -1,92 +1,107 @@
 package pk;
+
 import java.util.*;
 import java.util.logging.Logger;
 
-abstract class Calculator{
-    private static final Logger LOGGER =  Logger.getLogger("InfoLogging");
-    private int value;
-    abstract void add(int value);
-    abstract void sub(int value);
-    abstract void mul(int value);
-    abstract void div(int value);
-    abstract void rem(int value);
+abstract class Calculator {
+    private static final Logger LOGGER = Logger.getLogger("InfoLogging");
+    private double value;
 
-    public int getValue(){
+    abstract void add(double value);
+
+    abstract void sub(double value);
+
+    abstract void mul(double value);
+
+    abstract void div(double value);
+
+    abstract void rem(double value);
+
+    public double getValue() {
         return value;
     }
-    public void setValue(int val){
+
+    public void setValue(double val) {
         value = val;
     }
 }
 
-public class CalculatorDs extends Calculator{
-    void add(int value){
-        setValue(getValue()+value);
+public class CalculatorDs extends Calculator {
+    void add(double value) {
+        setValue(getValue() + value);
     }
-    void sub(int value){
-        setValue(getValue()-value);
+
+    void sub(double value) {
+        setValue(getValue() - value);
     }
-    void mul(int value){
-        setValue(getValue()*value);
+
+    void mul(double value) {
+        setValue(getValue() * value);
     }
-    void div(int value){
-        setValue(getValue()/value);
+
+    void div(double value) {
+        setValue(getValue() / value);
     }
-    void rem(int value){
-        setValue(getValue()%value);
+
+    void rem(double value) {
+        setValue(getValue() % value);
     }
-    static void print(){
-        String s = "Result: "+c.getValue();
+
+    static void prdouble() {
+        String s = "Result: " + c.getValue();
         LOGGER.info(s);
     }
-    private static final Logger LOGGER =  Logger.getLogger("InfoLogging");
+
+    private static final Logger LOGGER = Logger.getLogger("InfoLogging");
     static Calculator c = new CalculatorDs();
     static String msg = "Enter a value: ";
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n = 0;
-        do{
+        double n = 0;
+        do {
             LOGGER.info(msg);
-            int data = sc.nextInt();
+            double data = sc.nextDouble();
             c.setValue(data);
-            int l = 0;
-            do{
-                LOGGER.info("1.Add 2.subract 3.divide 4.multiply 5.clear 6.exit");
-                int m = sc.nextInt();
-                if(m == 1){
-                    LOGGER.info(msg);
-                    int value = sc.nextInt();
-                    c.add(value);
-                    print();
+            double l = 0;
+            do {
+                try {
+                    LOGGER.info("1.Add 2.subract 3.divide 4.multiply 5.clear 6.exit");
+                    double m = sc.nextDouble();
+                    if (m == 1) {
+                        LOGGER.info(msg);
+                        double value = sc.nextDouble();
+                        c.add(value);
+                        prdouble();
+                    } else if (m == 2) {
+                        LOGGER.info(msg);
+                        double value = sc.nextDouble();
+                        c.sub(value);
+                        prdouble();
+                    } else if (m == 3) {
+                        LOGGER.info(msg);
+                        double value = sc.nextDouble();
+                        c.div(value);
+                        prdouble();
+                    } else if (m == 4) {
+                        LOGGER.info(msg);
+                        double value = sc.nextDouble();
+                        c.mul(value);
+                        prdouble();
+                    } else if (m == 5) {
+                        c.setValue(0);
+                        l = 1;
+                    } else if (m == 6) {
+                        n = 1;
+                        l = 1;
+                    }
+                } catch (Exception e) {
+                    String s = ""+e;
+                    LOGGER.info(s);
+                    sc.nextLine();
                 }
-                else if(m == 2){
-                    LOGGER.info(msg);
-                    int value = sc.nextInt();
-                    c.sub(value);
-                    print();
-                }
-                else if(m == 3){
-                    LOGGER.info(msg);
-                    int value = sc.nextInt();
-                    c.div(value);
-                    print();
-                }
-                else if(m == 4){
-                    LOGGER.info(msg);
-                    int value = sc.nextInt();
-                    c.mul(value);
-                    print();
-                }
-                else if(m == 5){
-                    c.setValue(0);
-                    l = 1;
-                }
-                else if(m == 6){
-                    n=1;
-                    l = 1;
-                }
-            }while(l==0);
-        }while(n == 0);
-        
+            } while (l == 0);
+        } while (n == 0);
+
     }
 }
